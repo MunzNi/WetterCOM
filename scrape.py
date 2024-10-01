e2579cbfe119eff862fdd41b06b3aeffa184df95
MunzNi <munz.nils@live.de>
2024-10-01 16:43:33 +0200

    added window size back

--- scrape.py
+++ scrape.py
@@ -29,6 +29,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument(f"--user-agent={user_agent}")
 driver = webdriver.Chrome(options=chrome_options)
 
