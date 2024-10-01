9f49d34ee8280897ddf0fb145caab9b8a0a3fb28
MunzNi <munz.nils@live.de>
2024-10-01 15:35:40 +0200

    maybe fix for env variables missing in python script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 CRON_SCHEDULE=${CRON_SCHEDULE:-"0 * * * *"}
 echo "$CRON_SCHEDULE /usr/local/bin/python /app/scrape.py >> /var/log/cron.log 2>&1" | crontab -
+printenv | grep -v "no_proxy" >> /etc/environment
 cron && tail -f /var/log/cron.log
